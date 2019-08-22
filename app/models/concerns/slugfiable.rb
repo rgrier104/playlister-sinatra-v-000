a215814ac9c29453171f99330092ca8da42b043c
Rachel Grier <rgrier104@gmail.com>
2019-08-22 22:19:55 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugfiable.rb
+++ app/models/concerns/slugfiable.rb
@@ -1,10 +1,18 @@
-module slugifiable
+module Slugifiable
 
   module InstanceMethods
+    def slug
+      name = self.name.downcase.gsub(' ','-')
+    end
+
   end
 
   module ClassMethods
-
+    def self.find_by_slug(slug)
+      Artist.all.find do |artist|
+        artist.slug == slug
+      end
+    end
   end
 
 end
