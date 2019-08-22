412b96b6f11522e7eb9cf68e2d227856c9e9e53d
Rachel Grier <rgrier104@gmail.com>
2019-08-22 22:25:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,8 +29,8 @@ class SongsController < ApplicationController
       @artist = Artist.create(name: params[:artist][:name])
       @artist.songs << @song
     end
-binding.pry
-    redirect to "/songs/#{@song.slug}""
+
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
